2969298e1a99ce52c18a7e240149b293d684c094
FrankRuis <github@frank-ruis.nl>
2015-06-28 11:39:06 +0200

    Constants may now be constructed with a character

--- src/pp/cc/project/dataobjects/Sprockell/Constant.java
+++ src/pp/cc/project/dataobjects/Sprockell/Constant.java
@@ -6,6 +6,7 @@ package pp.cc.project.dataobjects.Sprockell;
  * A Sprockell constant
  */
 public class Constant extends Arg {
+    private boolean isChar;
     private int value;
 
     /**
@@ -14,11 +15,22 @@ public class Constant extends Arg {
      */
     public Constant(int i) {
         super(Type.Const);
+        this.isChar = false;
         this.value = i;
+    }
+
+    /**
+     * Construct a constant with the value of the given character
+     * @param c The character
+     */
+    public Constant(char c) {
+        super(Type.Const);
+        this.isChar = true;
+        this.value = (int) c;
     }
 
     @Override
     public String toString() {
-        return String.format("(%d)", value);
+        return isChar ? String.format("(ord '%c')", (char) value) : String.format("(%d)", value);
     }
 }
